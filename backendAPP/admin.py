1692393f5406c0806136da51ad07389e344f0f12
ZSY <1169846461@qq.com>
2018-02-10 20:10:23 +0800

    zsy master learn backend

--- backendAPP/admin.py
+++ backendAPP/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import person
 
 class personAdmin(admin.ModelAdmin):
-	list_display = ('name', 'age',)
-print("****** admin! ******")
-admin.site.register(person)
+    list_display = ('name', 'age', 'composite_info',)
+
+class personAdminComposite(admin.ModelAdmin):
+    list_display = ('composite_info',)
+
+admin.site.register(person, personAdminComposite)
+
+print("****** admin is running ******")
--- backendAPP/models.py
+++ backendAPP/models.py
@@ -2,8 +2,24 @@
 from django.db import models
 
 class person(models.Model):
-	name = models.CharField(max_length = 30)
-	age = models.IntegerField()
+    name = models.CharField(max_length = 30)
+    age = models.IntegerField()
+    # position = models.CharField(max_length = 50)
 
-	def __str__(self):
-	    return self.name
+    def composite(self):
+        return self.name + ' GOOD! '
+
+    composite.short_description = "Name and Age:"
+
+    composite_info = property(composite)
+
+    def __str__(self):
+        return self.name
+
+print("****** models is running ******")
+
+
+
+
+
+
